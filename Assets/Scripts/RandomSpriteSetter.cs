f055acefced9195a33f224055e2a268ab786e683
Marcel Pfaffhauser <tigris@gmx.ch>
2019-01-27 13:20:01 +0100

    Score geändert und prefabs mit teller erweitert

---
+++ Assets/Scripts/RandomSpriteSetter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpriteSetter : MonoBehaviour {
+
+    public GameObject[] Sprites;
+	// Use this for initialization
+	void Start () {
+        int rnd = Random.Range(0, Sprites.Length);
+        if (Sprites[rnd]==null) {
+            return;
+        }
+        for (int i = 0; i < Sprites.Length; i++) {
+            if (Sprites[i]!=null) {
+                Sprites[i].SetActive(false);
+            }
+        }
+        Sprites[rnd].SetActive(true);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
